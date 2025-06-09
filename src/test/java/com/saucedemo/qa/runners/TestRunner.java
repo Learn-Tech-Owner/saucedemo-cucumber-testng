a5bfeda83e41f6098684536cf7e78535c8f1878b
Mozart QA <mozart.kiangoyi@amani.consulting>
2025-06-09 16:53:36 +0000

    Test

--- src/test/java/com/saucedemo/qa/runners/TestRunner.java
+++ src/test/java/com/saucedemo/qa/runners/TestRunner.java
@@ -2,7 +2,6 @@ package com.saucedemo.qa.runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/resources/features", // Chemin vers les fichiers .feature
