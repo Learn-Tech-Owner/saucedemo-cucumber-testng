2896900bcd6b6cf508677f8b73cde6b53126c385
Mozart QA <mozart.kiangoyi@amani.consulting>
2025-06-09 17:10:14 +0000

    Disable monochrome to runner

--- src/test/java/com/saucedemo/qa/runners/LoginTestRunner.java
+++ src/test/java/com/saucedemo/qa/runners/LoginTestRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.testng.CucumberOptions;
                 "html:target/cucumber-reports/cucumber-html-report.html", // Rapport HTML
                 "json:target/cucumber-reports/cucumber.json", // Rapport JSON
                 "rerun:target/cucumber-reports/rerun.txt"}, // Pour re-exécuter les tests qui ont échoué
-        monochrome = true, // Rend la console plus lisible
+        //monochrome = true, // Rend la console plus lisible
         tags = "@login" // Exécution des scénarios de fonctionnalité de gestion de panier d'achat
 )
 public class LoginTestRunner extends AbstractTestNGCucumberTests {}
